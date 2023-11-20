ace76c16d26ca654608cba5a5217daa6509373be
JCamiloRamirezTabares <juancaram21@gmail.com>
2023-11-20 17:33:31 -0500

    Fix login

--- React/src/routes/index.jsx
+++ React/src/routes/index.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import axios from  '../config/axios'
 
-import BookList from '../pages/BookList';
 import Login from '../pages/Login';
 import Home from '../pages/Home';
 import AuthorsPage from '../pages/AuthorsPage';
@@ -14,34 +13,36 @@ const isUserAuthenticated = () => {
 
     if (token) {
         // Aquí un ejemplo utilizando axios
-        axios.get('/verifyToken')
+          axios.get('/verifyToken')
             .then(response => {
                 const authenticationStatus = response.headers['authentication-status'];
                 console.log("El token es valido")
-                return authenticationStatus === 'Valid';
+                return true;
             })
             .catch(error => {
                 console.log("El token no es valido")
                 console.error('Error al verificar el token:', error);
                 return false;
             });
+    } else{
+        console.log("Actualmente no hay token")
+        return false;
     }
-
-    return false;
 };
 
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/books"
-             element={<PrivateRoute element={<BookList />} isAuthorized={isUserAuthenticated()} />}
-      />
       <Route path="/*" element={<Login />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/books"
+             element={<PrivateRoute element={<BooksPage />} isAuthorized={isUserAuthenticated()} />}
+      />
+      <Route path="/authors"
+             element={<PrivateRoute element={<AuthorsPage />} isAuthorized={isUserAuthenticated()} />}
+      />
       <Route path="/home" element={<Home />} />
-      <Route path="/AuthorsPage" element={<AuthorsPage />} />
-      <Route path="/BooksPage" element={<BooksPage />} />
     </Routes>
   </BrowserRouter>
 );
